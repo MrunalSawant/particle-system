d90c1a5c08f2a8954c4b58551a2ae3abe2bc6c45
Mrunal Sawant <mrunal.sawant>
2022-06-11 18:32:39 +0530

    Remove unneccessary variable

--- src/Experience/World.ts
+++ src/Experience/World.ts
@@ -53,28 +53,23 @@ export default class World {
     const positions = new Float32Array(numPoints * 3);
     const colors = new Float32Array(numPoints * 3);
 
-    let k = 0;
-
     for (let i = 0; i < length; i += 1) {
       const x = Math.random() * 10;
       const y = Math.random() * 10;
-      const z = Math.random() * -10;
+      const z = Math.random() * -20;
 
-      positions[3 * k] = x;
-      positions[3 * k + 1] = y;
-      positions[3 * k + 2] = z;
+      positions[3 * i] = x;
+      positions[3 * i + 1] = y;
+      positions[3 * i + 2] = z;
 
       const intensity = Math.random() * 5;
-      colors[3 * k] = color.r * intensity;
-      colors[3 * k + 1] = color.g * intensity;
-      colors[3 * k + 2] = color.b * intensity;
-
-      k += 1;
+      colors[3 * i] = color.r * intensity;
+      colors[3 * i + 1] = color.g * intensity;
+      colors[3 * i + 2] = color.b * intensity;
     }
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
     geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-    geometry.computeBoundingBox();
 
     return geometry;
   }
@@ -84,31 +79,22 @@ export default class World {
     const numPoints = length;
     const indices = new Uint16Array(numPoints);
 
-    let k = 0;
-
     for (let j = 0; j < length; j += 1) {
-      indices[k] = k;
-      k += 1;
+      indices[j] = j;
     }
 
     geometry.setIndex(new THREE.BufferAttribute(indices, 1));
 
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load(StarTexture);
-    const material = new THREE.PointsMaterial({ size: PointSize, vertexColors: true, map: texture });
+    const material = new THREE.PointsMaterial({
+      size: PointSize, vertexColors: true, map: texture, blending: THREE.AdditiveBlending
+    });
 
     return new THREE.Points(geometry, material);
   }
 
   private setWorld(): void {
-    // const geometry = new THREE.PlaneGeometry(1, 1, 1);
-    // const material = new THREE.PointsMaterial({ color: 0x00ff00, size: 0.5 });
-    // const cube = new THREE.Points(geometry, material);
-
-    // const edges = new THREE.EdgesGeometry(geometry);
-    // const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }));
-    // this.experience.scene.add(cube);
-    // this.experience.scene.add(line);
   }
 
   public update(): void {
